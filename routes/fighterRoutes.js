89f730f1e9ed5f25434f85624d7e4bab8d2f8aea
Tamskray <tamskray25@gmail.com>
2025-05-13 06:53:09 +0300

    added: crud for fighter

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -8,6 +8,76 @@ import {
 
 const router = Router();
 
-// TODO: Implement route controllers for fighter
+router.get(
+  "/",
+  (req, res, next) => {
+    try {
+      const fighters = fighterService.getAllFighters();
+      res.locals.data = fighters;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  },
+  responseMiddleware
+);
+
+router.get(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const fighter = fighterService.getFighterById(req.params.id);
+      res.locals.data = fighter;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  },
+  responseMiddleware
+);
+
+router.post(
+  "/",
+  createFighterValid,
+  (req, res, next) => {
+    try {
+      const fighter = fighterService.create(req.body);
+      res.locals.data = fighter;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  },
+  responseMiddleware
+);
+
+router.patch(
+  "/:id",
+  updateFighterValid,
+  (req, res, next) => {
+    try {
+      const updated = fighterService.update(req.params.id, req.body);
+      res.locals.data = updated;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  },
+  responseMiddleware
+);
+
+router.delete(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const deleted = fighterService.delete(req.params.id);
+      res.locals.data = deleted;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  },
+  responseMiddleware
+);
 
 export { router };
--- services/fighterService.js
+++ services/fighterService.js
@@ -1,7 +1,58 @@
 import { fighterRepository } from "../repositories/fighterRepository.js";
 
 class FighterService {
-  // TODO: Implement methods to work with fighters
+  getAllFighters() {
+    return fighterRepository.getAll();
+  }
+  getFighterById(id) {
+    const fighter = fighterRepository.getOne({ id });
+    if (!fighter) throw new Error("Fighter not found");
+    return fighter;
+  }
+
+  create(data) {
+    const existing = fighterRepository.getOne({
+      name: data.name.toLowerCase(),
+    });
+
+    if (existing) {
+      throw new Error("Fighter with this name already exists");
+    }
+
+    const fighter = {
+      ...data,
+      name: data.name.toLowerCase(),
+      health: data.health ?? 85,
+    };
+
+    return fighterRepository.create(fighter);
+  }
+
+  update(id, data) {
+    const fighter = fighterRepository.getOne({ id });
+    if (!fighter) throw new Error("Fighter not found");
+
+    if (data.name) {
+      const existing = fighterRepository.getOne({
+        name: data.name.toLowerCase(),
+      });
+      if (existing && existing.id !== id) {
+        throw new Error("Fighter with this name already exists");
+      }
+      data.name = data.name.toLowerCase();
+    }
+
+    return fighterRepository.update(id, data);
+  }
+
+  delete(id) {
+    const fighter = fighterRepository.getOne({ id });
+    if (!fighter) {
+      throw new Error("Fighter not found");
+    }
+
+    return fighterRepository.delete(id);
+  }
 }
 
 const fighterService = new FighterService();
